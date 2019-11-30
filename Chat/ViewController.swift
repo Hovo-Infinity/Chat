01214ade960c56193fe8504f8615d6c7acf8da28
Hovhannes Stepanyan <hovhannes@helix.am>
2019-11-30 09:15:13 +0400

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Hovhannes Stepanyan on 11/30/19.
+//  Copyright © 2019 Hovhannes Stepanyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
